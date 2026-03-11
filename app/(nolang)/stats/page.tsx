5c490e0d2c6c67491fa66e0e1523af5d0e96e54e
Remi BONNET <bonnet@fanaticalhelp.com>
2026-03-11 16:00:41 +0100

    feat: Add active installs

--- app/(nolang)/stats/page.tsx
+++ app/(nolang)/stats/page.tsx
@@ -5,6 +5,8 @@ const StatsPage: React.FC = async () => {
     const githubapi = GitHubAPI.getInstance()
 
     const releases = (await githubapi.getReleases()).filter(x => x.prerelease == false)
+    const ha_analytics = await (await fetch('https://analytics.home-assistant.io/custom_integrations.json')).json()
+    const veratile_count = ha_analytics.versatile_thermostat.versions
 
     const data: ReleaseInfos[] = []
 
@@ -13,7 +15,8 @@ const StatsPage: React.FC = async () => {
         console.log(assets);
         data.push({
             tag_name: release.tag_name,
-            downloads: assets && assets.length > 0 ? assets[0].download_count : 0
+            downloads: assets && assets.length > 0 ? assets[0].download_count : 0,
+            installs: veratile_count[release.tag_name] ?? 0
         })
     }
 
