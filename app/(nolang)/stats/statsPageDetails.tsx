82cc665858f4dc90024d7469ef57fc8f1b38963c
Remi BONNET <bontiv@users.noreply.github.com>
2026-03-16 15:37:07 +0100

    feat: donwloads stats page (#39)
    * feat: Download statistics of Versatile Thermostat
    
    * feat: Add active installs
    
    * fix: Fix some warning on stat page

---
+++ app/(nolang)/stats/statsPageDetails.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import Chart from "react-apexcharts";
+
+export type ReleaseInfos = {
+    tag_name: string,
+    downloads: number,
+    installs: number
+}
+
+const StatsPageDetails: React.FC<{ data: ReleaseInfos[] }> = ({ data }) => {
+    const formater = new Intl.NumberFormat('fr', {})
+
+    const series = [
+        {
+            name: 'Downloads',
+            type: 'line',
+            data: data.map(x => ({ x: x.tag_name, y: x.downloads })).reverse()
+        },
+        {
+            name: 'Actives Installs',
+            data: data.map(x => ({ x: x.tag_name, y: x.installs })).reverse()
+        }
+    ]
+
+    return <main className="container mx-auto">
+        <h1>Statistiques Versatile Thermostat</h1>
+        <Chart
+            series={series}
+            type="bar"
+            height={400}
+            options={{
+                chart: {
+                    id: 'mainchart',
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: false,
+                    }
+                },
+                stroke: {
+                    width: 1,
+                    curve: 'smooth'
+                },
+                yaxis: [
+                    {
+                        seriesName: 'Downloads',
+                        labels: {
+                            style: {
+                                colors: 'blue'
+                            }
+                        }
+                    },
+                    {
+                        seriesName: 'Actives Installs',
+                        opposite: true,
+                        labels: {
+                            style: {
+                                colors: 'green'
+                            }
+                        }
+                    }
+                ]
+            }}
+        />
+        <table border={1} className="border">
+            <thead>
+                <tr>
+                    <th className="px-4">Version</th>
+                    <th className="px-4">Téléchargements</th>
+                    <th className="px-4">Actives</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(relinfo =>
+                    <tr className="border-t" key={relinfo.tag_name}>
+                        <td className="px-4">{relinfo.tag_name}</td>
+                        <td className="px-4 text-right">{formater.format(relinfo.downloads)}</td>
+                        <td className="px-4 text-right">{formater.format(relinfo.installs)}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    </main>
+}
+
+
+export default StatsPageDetails;
